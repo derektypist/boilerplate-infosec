01d2e67036b9b3235ffd7ec0e06ed0af836f561b
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-03-02 11:01:34 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -10,6 +10,9 @@ var ninetyDaysInMilliseconds = 90*24*60*60*1000;
 app.use(helmet.hsts({maxAge: ninetyDaysInMilliseconds, force: true}));
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({
+  directives: {defaultSrc: ["'self'"], scriptSrc: ["'self'","trusted-cdn.com"]}
+}));
 
 
 
